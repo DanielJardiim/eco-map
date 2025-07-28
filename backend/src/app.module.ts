a17dd97149ac273ce790037e18c6a2a0bbdb487d
DanielJardiim <jn_dani@outlook.com>
2025-07-28 19:23:23 -0300

    chore(backend): conecta AppModule com Database e ProjectsModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './shared/database/database.module';
+import { ProjectsModule } from './modules/projects/projects.module';
 
 @Module({
-  imports: [DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    ProjectsModule,
+  ],
 })
 export class AppModule {}
