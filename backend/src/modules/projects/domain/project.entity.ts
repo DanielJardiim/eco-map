73dc59f835de8286ae7856281308772df72a7c68
DanielJardiim <jn_dani@outlook.com>
2025-07-28 19:27:02 -0300

    cria entidade Project

---
+++ backend/src/modules/projects/domain/project.entity.ts
@@ -0,0 +1,36 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { ProjectStatus } from './project-status.enum';
+import { ApiProperty } from '@nestjs/swagger';
+import { Geometry } from 'geojson';
+
+@Entity('projects')
+export class Project {
+  @ApiProperty()
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ApiProperty()
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
+
+  @ApiProperty({ enum: ProjectStatus })
+  @Column({ type: 'enum', enum: ProjectStatus })
+  status: ProjectStatus;
+
+  @ApiProperty()
+  @Column({ type: 'varchar', length: 255 })
+  responsibleResearcher: string;
+
+  @ApiProperty({ description: 'GeoJSON geometry' })
+  @Column({ type: 'jsonb' })
+  geometry: Geometry;
+
+  @ApiProperty()
+  @Column()
+  createdAt: Date;
+}
