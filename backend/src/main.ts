7250e42896078feb3917adffed90a9d28af9946f
DanielJardiim <jn_dani@outlook.com>
2025-07-30 13:13:08 -0300

    feat(backend): configura CORS para permitir requisições do frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,6 +8,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+  });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   const config = new DocumentBuilder()
