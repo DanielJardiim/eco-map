c9eae30af3e3547ca89aa0cb253d90c131aca6fa
DanielJardiim <jn_dani@outlook.com>
2025-07-30 14:35:09 -0300

    chore(frontend): adiciona declaração de módulo para CSS e importa CSS do Leaflet

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import "leaflet/dist/leaflet.css";
+
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
