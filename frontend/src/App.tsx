dc1fe947d0d9ae8636ebf82441a8cefde0ff7ddd
DanielJardiim <jn_dani@outlook.com>
2025-07-31 11:07:51 -0300

    feat(frontend): configura estilização global e atualiza App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ProjectsList } from "./features/projects/ProjectsList";
 import { Home } from "./pages/Home";
-import { Layout } from "./layouts/Layout";
 
 export function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <Layout>
-              <Home />
-            </Layout>
-          }
-        />
-        <Route path="/projects" element={<ProjectsList />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
